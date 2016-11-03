7c87b0e313a9b8de262107e23a5090c8cd2eb983
Brian Gates <brian.gates@theironyard.com>
2016-11-03 01:08:36 -0400

    fix preflight issue

--- proxy.rb
+++ proxy.rb
@@ -6,6 +6,14 @@ configure do
   enable :cross_origin
 end
 
+options "*" do
+  response.headers["Allow"] = "HEAD,GET,PUT,POST,DELETE,OPTIONS"
+
+  response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
+
+  200
+end
+
 get "/" do
   "hello world"
 end
