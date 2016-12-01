161f0d6cae6074aa2cdd536ce195948e336a5089
Brian Gates <brian.gates@theironyard.com>
2016-11-30 22:51:46 -0500

    struggle w Net:HTTP

--- proxy.rb
+++ proxy.rb
@@ -33,18 +33,14 @@ end
 
 get "/darksky/*" do
   cross_origin
-  logger.info "got past cross_origin"
   uri = URI("https://api.darksky.net/#{params['splat'][0]}")
-  logger.info "uri is #{uri}"
   content_type 'application/json', :charset => 'utf-8'
-  logger.info "content_type check"
+
   Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
-    logger.info "in the block"
     request = Net::HTTP::Get.new uri
-    logger.info "request is #{request}"
     response = http.request request # Net::HTTPResponse object
     logger.info "fn response is #{response}"
   end
   logger.info "out of block, response is #{response}"
-  response
+  response.class
 end
